09f5dd8c683e862d370790fee326c9a5b6d40a67
Matthew Anderson <30393@longroad.ac.uk>
2014-10-01 11:06:59 +0100

    SQL tasks upto 8

---
+++ select_existing_products.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def select_all_products():
+    with sqlite3.connect("coffee_shop.db") as db:
+        cursor = db.cursor()
+        cursor.execute("select * from Product")
+        products = cursor.fetchall()
+        return products
+
+def select_product(id):
+    with sqlite3.connect("coffee_shop.db") as db:
+        cursor = db.cursor()
+        cursor.execute("select name,price from Product where ProductID=?",(id,))
+        product = cursor.fetchone()
+        return product
+if __name__ == "__main__":
+    products = select_all_products()
+    print(products)
+    product = select_product(5)
+    print(product)
